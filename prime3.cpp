32578209d88d83a1bee8edefbeec81c993c7dc39
krishna <59382704+tandavakrishna@users.noreply.github.com>
2021-05-12 19:03:50 +0530

    Create prime3.cpp

---
+++ prime3.cpp
@@ -0,0 +1,76 @@
+  
+/* #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,flag=0;
+    cin>>n;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+        
+    }
+    if(flag==0)
+        {
+            cout<<"the number is prime";
+        }
+        else
+        {
+            cout<<"the number is not prime ";
+        }
+    return 0;
+}  
+  
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,flag=0;
+    cin>>n;
+    for(i=2;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+        
+    }
+    if(flag==0)
+        {
+            cout<<"the number is prime";
+        }
+        else
+        {
+            cout<<"the number is not prime ";
+        }
+    return 0;
+}*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,flag=0;
+    cin>>n;
+    for(i=2;i<=sqrt(n+1)/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+        
+    }
+    if(flag==0)
+        {
+            cout<<"the number is prime";
+        }
+        else
+        {
+            cout<<"the number is not prime ";
+        }
+    return 0;
