a7a1d8c8dcc4c146646ff62e24cb6054935ddd53
krishna <59382704+tandavakrishna@users.noreply.github.com>
2021-05-12 08:33:44 +0530

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,33 +1,30 @@
-#include<iostream>
+#include <bits/stdc++.h> 
+using   namespace std;
+
+#include<bits/stdc++.h>
+
 using namespace std;
+
 int main()
-{
-int n,r,oc,ec;
 
-cin>>n;
+{
 
-while(n>0)
-{
-r = n%10;
-if (r%2==0)
-{
-ec+=1;
-}
-else{
-oc+=1;
-}
-n=n/10;
-}
-if(oc==0)
-{
-cout<<"stron even"<<endl;
-}
-else if (ec==0)
-{
-cout<<"strong odd"<<endl;
-}
-else
-{
-cout<<"mixed number"<<endl;
-}
+    int n,res=0,u;
+
+    cin>>n;
+
+    while(n!=0)
+
+    {
+
+        u=n%10;
+
+        n=n/10;
+
+        res=res*10+u;
+
+    }
+
+    cout<<endl<<res;
+
 }
