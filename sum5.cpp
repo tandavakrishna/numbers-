498ba811741fc4586d722a8cbb7f2746e936d67a
krishna <59382704+tandavakrishna@users.noreply.github.com>
2021-06-11 09:15:51 +0530

    Create sum5.cpp

---
+++ sum5.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,sum,k;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum=0;
+        for(k=i;k<n;k++)
+        {
+            sum+=arr[k];
+        }
+        if(sum==5)
+        {
+            cout<<"true";
+        }
+    
+    }
+}
